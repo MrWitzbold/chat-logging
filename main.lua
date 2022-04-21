2c0f37ae42e0628e11b0417ad8c1852133a84464
Villinger <80601981+MrWitzbold@users.noreply.github.com>
2022-04-21 01:51:36 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 for i,v in pairs(game.Players:GetChildren()) do
     v.Chatted:connect(function(Message)
         username = v.Name
-        content = (username .. ": " .. Message)
+        content = ("\n" .. username .. ": " .. Message)
         setclipboard(content)
     end)
 end
